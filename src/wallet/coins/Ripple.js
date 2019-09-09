c05cb47a911af8cea2d078454bf63da5ac822ae8
ult-zhiwei <zhiwei.zheng@ultiledger.io>
2019-09-09 12:13:47 +0800

    add timeout in ripple

--- src/wallet/coins/Ripple.js
+++ src/wallet/coins/Ripple.js
@@ -18,7 +18,23 @@ class RippleWallet{
     this.url = url;
     if (!this.server || !this.server.isConnected()) {
       this.server = new RippleAPI({
-        server: url,maxFeeXRP: '0.05'
+        server: url, maxFeeXRP: '0.05',timeout:6000
+      });
+      this.server.on('error', (errorCode, errorMessage) => {
+        console.log(errorCode + ': ' + errorMessage);
+      });
+      this.server.on('connected', () => {
+        console.log('RippleApi connected');
+      });
+      this.server.on('disconnected', (code) => {
+        if (code !== 1000) {
+          console.log('Connection is closed due to error.');
+        } else {
+          console.log('Connection is closed normally.');
+        }
+      });
+      this.server.on('ledger', ledger => {
+        console.log('Rippleledger',JSON.stringify(ledger.hash, null, 2));
       });
     }
   }
