d0828c851fe6a9708f3226a6385ca9e2f455a1e5
jimjay <1535481943@qq.com>
2019-12-10 11:38:56 +0800

    修改发现页面的样式

--- src/pages/find/application-item.vue
+++ src/pages/find/application-item.vue
@@ -5,17 +5,16 @@
         @click.native="to(item)"
         class="item-block"
         v-for="(item, index) in datas"
-        :key="index">
+        :key="index"
+      >
         <div slot="title" class="card-title">
           <div class="normal-font">{{item.title}}</div>
           <div class="small-font text-ellipsis text-main">{{item.subTitle}}</div>
         </div>
         <div slot="thumb">
-          <img :src="item.imgUrl" width="60" height="60">
+          <img :src="item.imgUrl" width="60" height="60" />
         </div>
-        <div slot="desc" class="text-muted">
-          {{item.desc}}
-        </div>
+        <div slot="desc" class="text-muted">{{item.desc}}</div>
         <!--<div slot="footer" style="position: relative;top: -35px;">
           <van-icon name="arrow"></van-icon>
         </div>-->
@@ -24,44 +23,43 @@
   </pl-content-block>
 </template>
 <script>
-  import customerBrowser from 'core/utils/customerBrowser';
-  export default {
-    props: {
-      datas: Array
-    },
-    data () {
-      return {};
-    },
-    methods: {
-      to (item) {
-        customerBrowser.open(item.title, item.link);
-      }
+import customerBrowser from "core/utils/customerBrowser";
+export default {
+  props: {
+    datas: Array
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    to(item) {
+      customerBrowser.open(item.title, item.link);
     }
-  };
+  }
+};
 </script>
 <style lang="scss" scoped>
-  @import '~assets/scss/_variables.scss';
-  .application {
-    margin-top: 15px;
-    background: $main-background-color;
-  }
+@import "~assets/scss/_variables.scss";
+.application {
+  margin-top: 15px;
+  background: $main-background-color;
+}
 </style>
 <style lang="scss">
-  @import '~assets/scss/_variables.scss';
-  .application {
-    .van-card {
-      background: #fff;
-      padding: 5px 15px 5px 100px;
-      border-radius: $border-radius;
-      .van-card__thumb {
-        display:flex;
-        display: -webkit-flex;
-        align-items:center;
-
-      }
-    }
-    .card-title {
-      margin-top: 10px;
+@import "~assets/scss/_variables.scss";
+.application {
+  .van-card {
+    background: #fff;
+    padding: 5px 15px;
+    border-radius: $border-radius;
+    .van-card__thumb {
+      display: flex;
+      display: -webkit-flex;
+      align-items: center;
     }
   }
+  .card-title {
+    margin-top: 10px;
+  }
+}
 </style>
